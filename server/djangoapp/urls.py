44fca227c07e28568ce27a409b75813e90ef7c4c
Sanda-Cyber <esterahirlet78@yahoo.es>
2024-06-24 18:26:02 -0400

    change made by Sanda-Cyber

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,82 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
-# from . import views
+from django.urls import path  
 
-app_name = 'djangoapp'
-urlpatterns = [
-    # # path for registration
+ 
 
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
+from django.conf.urls.static import static  
 
-    # path for dealer reviews view
+ 
 
-    # path for add a review view
+from django.conf import settings  
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
+
+from djangoapp.views import login_user   
+
+from . import views 
+
+
+ 
+
+ 
+
+  
+
+ 
+
+app_name = 'djangoapp'  
+
+ 
+
+urlpatterns = [  
+
+    path('logout/', views.logout_request, name='logout'), 
+ 
+
+    # # path for registration  
+    path('register/', views.register_view, name='register'),
+
+ 
+
+  
+
+ 
+
+    # path for login  
+
+ 
+
+    path('login/', login_user, name='login'),  
+
+ 
+
+    # path(route='login', view=views.login_user, name='login'),  
+
+    path(route='login', view=views.login_user, name='login'), 
+
+     
+
+ 
+
+  
+
+ 
+
+    # path for dealer reviews view  
+
+ 
+
+  
+
+ 
+
+    # path for add a review view  
+
+ 
+
+  
+
+ 
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  
+
+ 
